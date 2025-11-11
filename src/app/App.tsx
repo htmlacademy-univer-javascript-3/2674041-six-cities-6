1ba8bbdf1cc4c983abc62c46a699a3b2eec46f71
hairy-snake <mamaev.gosha99@gmail.com>
2025-11-11 05:09:46 +0500

    task

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,36 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AppRoutes from '@/src/route';
 import MainPage from '@/src/pages/MainPage';
+import LoginPage from '@/src/pages/LoginPage';
+import FavouritesPage from '@/src/pages/FavouritesPage';
+import OfferPage from '@/src/pages/OfferPage';
+import ErrNotFoundPage from '@/src/pages/ErrNotFoundPage';
+import PrivateRoute from '@/src/PrivateRoute';
+
+
 type Settings = {
     cardsCount: number;
 }
 
 
-const App: React.FC<Settings> = ({ cardsCount }: Settings) => <MainPage cardsCount={ cardsCount } />;
+const App: React.FC<Settings> = ({ cardsCount }: Settings) => (
+  <BrowserRouter>
+    <Routes>
+      <Route path={AppRoutes.Main} element={<MainPage cardsCount={cardsCount} />} />
+      <Route path={AppRoutes.Login} element={<LoginPage />} />
+      <Route
+        path={AppRoutes.Favorites}
+        element={
+          <PrivateRoute auth={false}>
+            <FavouritesPage />
+          </PrivateRoute>
+        }
+      />
+      <Route path={AppRoutes.Offer} element={<OfferPage />} />
+      <Route path="*" element={<ErrNotFoundPage />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
